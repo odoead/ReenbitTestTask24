d7e03d5ec0ebebd349d4a607afa3e73274ddad93
Kirill <muravjov.kirill@gmail.com>
2024-12-09 01:59:58 +0200

    set message component as a start page

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MessengerComponent } from './messenger/messenger.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MessengerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
