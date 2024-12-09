cd2977c587ec5628510ff5d7e26102133946b60e
Kirill <muravjov.kirill@gmail.com>
2024-12-09 12:54:32 +0200

    db migration

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'reenbittesttask2420241208030927.azurewebsites.net',
+  apiUrl: 'https://reenbittesttask2420241208030927.azurewebsites.net',
 };
---
+++ ReenbitTestTask24/Migrations/20241209103653_init.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReenbitTestTask24.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ChatMessages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Message = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChatMessages", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sentiments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SentimentType = table.Column<int>(type: "int", nullable: false),
+                    PositiveScore = table.Column<double>(type: "float", nullable: false),
+                    NegativeScore = table.Column<double>(type: "float", nullable: false),
+                    NeutralScore = table.Column<double>(type: "float", nullable: false),
+                    ChatMessageId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sentiments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Sentiments_ChatMessages_ChatMessageId",
+                        column: x => x.ChatMessageId,
+                        principalTable: "ChatMessages",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sentiments_ChatMessageId",
+                table: "Sentiments",
+                column: "ChatMessageId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sentiments");
+
+            migrationBuilder.DropTable(
+                name: "ChatMessages");
+        }
+    }
+}
